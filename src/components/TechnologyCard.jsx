f5bf153577889aedbc093977a3e7f7f7bcfa9ede
KG367 <anchouskostya@gmail.com>
2025-11-27 18:39:36 +0300

    Практика 21
    Включены логи

--- src/components/TechnologyCard.jsx
+++ src/components/TechnologyCard.jsx
@@ -1,12 +1,18 @@
 import "./TechnologyCard.css"
+import TechnologyNotes from "./TechnologyNotes";
 
-function TechnologyCard({ technology, chSt }) {
+function TechnologyCard({ technology, chSt, onUpdateNotes }) {
     return (
         <div className={`tech-card ${technology.status}`}>
             <h3>{technology.title}</h3>
             <p>{technology.description}</p>
             <p>Статус: {technology.status === "completed" ? '✅' : (technology.status === "in-progress" ? '⏳' : '❌')}</p>
-            <button onClick={()=>{chSt(technology.id)}}>Сменить статус</button>
+            <button onClick={() => { chSt(technology.id) }}>Сменить статус</button>
+            <TechnologyNotes
+                notes={technology.notes}
+                onNotesChange={onUpdateNotes}
+                techId={technology.id}
+            />
         </div>
     );
 }
